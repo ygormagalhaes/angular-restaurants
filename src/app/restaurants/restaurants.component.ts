48a4c0283185c121743172279ec9bf3804374d9f
Ygor Magalhães <emaildoygor@gmail.com>
2018-11-05 22:41:04 -0200

    Criação do componente que representa um restaurante

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant/restaurant.model';
 
 @Component({
   selector: 'mt-restaurants',
@@ -6,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsComponent implements OnInit {
 
-  constructor() { }
+  public restaurants: Restaurant[] = [
+    {
+      id: 'bread-bakery',
+      name: 'Bread & Bakery',
+      category: 'Bakery',
+      deliveryEstimate: '25m',
+      rating: 4.9,
+      imagePath: 'assets/img/restaurants/breadbakery.png'
+    },
+    {
+      id: 'burger-house',
+      name: 'Burger House',
+      category: 'Hamburgers',
+      deliveryEstimate: '100m',
+      rating: 3.5,
+      imagePath: 'assets/img/restaurants/burgerhouse.png'
+    }
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
