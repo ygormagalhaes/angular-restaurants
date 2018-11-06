3eeafc47f7d86d01b432ef55ec4d479fbb2d4523
Ygor Magalhães <emaildoygor@gmail.com>
2018-11-06 21:12:18 -0200

    Implementação de classe para tratar erros

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import { Http } from '@angular/http';
 import { MEAT_API } from '../app.api';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ErrorHandler } from 'app/app.error-handler';
 
 @Injectable()
 export class RestaurantsService {
@@ -12,7 +14,8 @@ export class RestaurantsService {
 
   public getRestaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
-    .map(response => response.json());
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError);
   }
 
 }
