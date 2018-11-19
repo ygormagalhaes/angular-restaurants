abe08a5cf5e8da1b9dcd8c759e30d1afb6dc6adc
Ygor Magalhães <emaildoygor@gmail.com>
2018-11-19 21:04:31 -0200

    Utilizando content projection para utilizar o componente input criado anteriormente

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
-  selector: 'mt-input',
+  selector: 'mt-input-container',
   templateUrl: './input.component.html'
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, AfterContentInit {
+
+  @Input() errorMessage: string;
+  @Input() label: string;
+
+  input: NgModel;
+
+  teste: string;
+
+  @ContentChild(NgModel) model: NgModel;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit(): void {
+    if (this.model === undefined) {
+      throw new Error('É necessário utilizar esse componente com um ngModel.');
+    } else {
+      this.input = this.model;
+    }
+  }
+
+  hasSucess(): boolean {
+    return this.input.valid && (this.input.touched || this.input.dirty);
+  }
+
+  hasError(): boolean {
+    return this.input.invalid && (this.input.touched || this.input.dirty);
   }
 
 }
