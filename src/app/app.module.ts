ba0f3abf8598442c1917284783671242f36c9b6e
Ygor Magalhães <emaildoygor@gmail.com>
2018-11-19 00:01:57 -0200

    Criação de componente de compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+import { OrderComponent } from './order/order.component';
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
     MenuComponent,
     ShoppingCartComponent,
     ReviewsComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
