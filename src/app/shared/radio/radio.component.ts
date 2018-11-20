8c9a0d3c6e46ed84141cb1d98d514d690f32ae81
Ygor Magalhães <emaildoygor@gmail.com>
2018-11-20 12:13:09 -0200

    Utilizando ControlValueAccessor para a comunicação do componente radio com order

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,23 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { RadioOption } from './radio-option.model';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
+
 
   @Input() options: RadioOption[];
 
   selectedValue: any;
+  onChange: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  writeValue(obj: any): void {
+    this.selectedValue = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+  }
+
   setValue(value: any) {
     this.selectedValue = value;
+    this.onChange(this.selectedValue);
   }
 
 }
